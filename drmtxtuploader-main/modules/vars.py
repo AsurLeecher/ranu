4e6faba150c50e643ed22f7f2198a109371cbd91
hackercoder24 <piyushyadv17147714@gmail.com>
2024-09-30 14:13:58 +0530

    Update vars.py

--- drmtxtuploader-main/modules/vars.py
+++ drmtxtuploader-main/modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 20491966
 api_hash = "aa1c8f86db7f78fe9bfdd77bb48a5b23"
-bot_token = "7499120950:AAGY3uq99uBtIfg0LqR22QAaRI__2UIMRBo"
+bot_token = "8079973322:AAHokCNQPFIlUTqRmsDwfPT9RHwOBL2Rmew"
 AUTH = 7168441486
 #Bot Username = "annukijaanbot"
