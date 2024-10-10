20385a6e007a8e684566e4be8d340e82eed93239
hackercoder24 <piyushyadv17147714@gmail.com>
2024-10-10 13:25:42 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 20491966
 api_hash = "aa1c8f86db7f78fe9bfdd77bb48a5b23"
-bot_token = "7860203060:AAF1JLPUqLpq4oJPAQEN8B_Vd-Kwn8n5J0Y"
+bot_token = "7568441688:AAGDPOEETIGFfTtpB2iVrkucaXZpicQlPwk"
 AUTH = 7168441486
 #Bot Username = "annukijaanbot"
